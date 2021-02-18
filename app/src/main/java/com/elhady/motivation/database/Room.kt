afc2fc551080d4680a15e12b03b57f63b4e2c04f
islamelhady <i.elhady44@gmail.com>
2021-02-18 18:14:13 +0200

    Add VideoDao

--- app/src/main/java/com/elhady/motivation/database/Room.kt
+++ app/src/main/java/com/elhady/motivation/database/Room.kt
@@ -1,7 +1,20 @@
 package com.elhady.motivation.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.*
+import androidx.room.Query
+
 /**
  * Created by islam elhady on 16-Feb-21.
  */
-class Room {
+@Dao
+interface VideoDao {
+
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = REPLACE)
+    fun insertAll( videos: List<DatabaseVideo>)
 }
