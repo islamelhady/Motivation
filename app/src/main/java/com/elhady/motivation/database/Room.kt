542f219362191466ccbeb9c5edf4777dfd85301f
islamelhady <i.elhady44@gmail.com>
2021-02-20 20:28:38 +0200

    Implement RoomDatabase

--- app/src/main/java/com/elhady/motivation/database/Room.kt
+++ app/src/main/java/com/elhady/motivation/database/Room.kt
@@ -1,10 +1,9 @@
 package com.elhady.motivation.database
 
+import android.content.Context
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
+import androidx.room.*
 import androidx.room.OnConflictStrategy.*
-import androidx.room.Query
 
 /**
  * Created by islam elhady on 16-Feb-21.
@@ -17,4 +16,22 @@ interface VideoDao {
 
     @Insert(onConflict = REPLACE)
     fun insertAll( videos: List<DatabaseVideo>)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideosDatabase: RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
 }
