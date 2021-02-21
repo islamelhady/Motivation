d0392544d13f14ada358de94979fc90cd35f2648
islamelhady <i.elhady44@gmail.com>
2021-02-21 17:31:07 +0200

    Retrieve data from the database

--- app/src/main/java/com/elhady/motivation/repository/VideosRepository.kt
+++ app/src/main/java/com/elhady/motivation/repository/VideosRepository.kt
@@ -1,6 +1,10 @@
 package com.elhady.motivation.repository
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.elhady.motivation.database.VideosDatabase
+import com.elhady.motivation.database.asDomainModel
+import com.elhady.motivation.domain.DevByteVideo
 import com.elhady.motivation.network.DevByteNetwork
 import com.elhady.motivation.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -11,6 +15,10 @@ import timber.log.Timber
  * Created by islam elhady on 16-Feb-21.
  */
 class VideosRepository (private val database: VideosDatabase){
+
+    val videos: LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
 
     suspend fun refreshVideos(){
         withContext(Dispatchers.IO){
